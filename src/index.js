1942847656d6b69a15921bc4c68344c5943bf069
ariva-dev <ariva.dev@gmail.com>
2020-07-28 22:12:32 +0700

    Add support for style prop

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {Icons} from "./map"
 
-export function Icon({name, size, color, fill, strokeWidth, borderWidth}) {
+export function Icon({name, size, color, fill, strokeWidth, style, borderWidth}) {
   return Icons[name]({
     height: size,
     width: size,
     fill,
     color,
+    style,
     strokeWidth
    // borderWidth: borderWidth
   });
